ba0fe31daf747d075785914f497b776d300fc7f5
V. Mikhail <vemant81@gmail.com>
2025-05-19 22:28:11 +0300

    Diplom_4 3 added inizialization

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/test/AppAutorizationTest.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/test/AppAutorizationTest.java
@@ -1,21 +1,11 @@
 package ru.iteco.fmhandroid.ui.test;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
-import static androidx.test.espresso.matcher.ViewMatchers.withHint;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
-import static ru.iteco.fmhandroid.ui.data.DataHelper.waitDisplayed;
-
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import ru.iteco.fmhandroid.R;
 import ru.iteco.fmhandroid.ui.AppActivity;
 import ru.iteco.fmhandroid.ui.page.LoginPage;
 import ru.iteco.fmhandroid.ui.page.MainPage;
@@ -28,6 +18,12 @@ public class AppAutorizationTest {
     @Rule
     public ActivityScenarioRule<AppActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(AppActivity.class);
+
+    @Before
+    void setup {
+        var loginPage = open("ru.iteco.fmhandroid", LoginPage.class);
+    }
+
 
     @Test
     public void validLoginValidPasswordTest() {
